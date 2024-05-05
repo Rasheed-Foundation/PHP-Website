36566e53dc07070d9a8c9f5aae4a61d72865c3bb
Rasheed Foundation <153905773+Rasheedfoundation2@users.noreply.github.com>
2024-05-05 22:37:49 +0530

    Update z_db.php
    db update

--- dashboard/z_db.php
+++ dashboard/z_db.php
@@ -1,19 +1,29 @@
 <?php
-$con = new mysqli("localhost", "root", "", "rasheed-foundation");
+$host = 'l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username = 'ow19auwyd94iw9mw';
+$password = 'qgk9ho9jvrc43iar';
+$database = 'eymvidpjksf4tewt';
+$port = 3306;
+
+// Create connection
+$con = new mysqli($host, $username, $password, $database, $port);
+
+// Check connection
 if ($con->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    echo "Failed to connect to MySQL: (" . $con->connect_errno . ") " . $con->connect_error;
+} else {
+    echo "Connected successfully";
 }
 
-//Your Website URL Goes Here
-$url="http://localhost/vogue";
+// Your Website URL Goes Here
+$url = "https://rasheedfoundation.org";
 
-
-//Set Blog Activation Bonus Here (It must be only Number)
-$blog_bonus ="10";
-//Set Article Activation Bonus Here (It must be only Number)
-$art_bonus="10";
-//Set Daily Login Bonus Here (It must be only Number)
-$login_bonus="10";
-//Set Currency Symbol for daily login bonus Here
-$money="$";
+// Set Blog Activation Bonus Here (It must be only Number)
+$blog_bonus = "10";
+// Set Article Activation Bonus Here (It must be only Number)
+$art_bonus = "10";
+// Set Daily Login Bonus Here (It must be only Number)
+$login_bonus = "10";
+// Set Currency Symbol for daily login bonus Here
+$money = "$";
 ?>
